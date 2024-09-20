d16f680172d9855e20f911a2ed11be45f02308af
M618w <tahaacngz12@gmail.com>
2024-09-20 20:19:13 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+
+print("_CALCULATOR_")
+question=input("What process do you want to do?")
+
+if question == "multiplication":
+    sayi1=int(input("Write the first number"))
+    sayi2=int(input("Write the second number."))
+    print(sayi1*sayi2)
+elif question == "addition":
+    sayi3=int(input("Write the first number."))
+    sayi4=int(input("Write the second number."))
+    print(sayi3+sayi4)
+elif question == "extraction":
+    sayi5=int(input("Write the first number."))
+    sayi6=int(input("Write the second number."))
+    print(sayi5-sayi6)
+elif question == "division":
+    sayi7=int(input("Write the first number."))
+    sayi8=int(input("Write the second number."))
+    print(sayi7/sayi8)
+
+
+
+
+
